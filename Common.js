41186872968053eafdc10e8ea2181a95d03404d4
kyojuceles <kyojuceles@gmail.com>
2013-05-08 20:34:04 +0900

    - private 멤버를 추가하는 프롵토타입 - 객체에 기능을 추가하는 함수. - 예제 html 문서 추가

--- Common.js
+++ Common.js
@@ -32,4 +32,13 @@ Function.method('inherits', function (Parent)
     	this.prototype = new Parent();
     	return this;
 });
-/////////////////////////////////////////////////////////////////////////////
+
+Object.method('superior', function (name)
+{
+	var that = this,
+		method = that[name];
+	return function () {
+		return method.apply(that, arguments);
+	};
+});
+///////////////////////////////////////////////////////////////////////////
